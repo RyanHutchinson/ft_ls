238e6d5ab6375949e338ab82068fd9945270f138
Ryan HUTCHINSON <rhutchin@student.wethinkcode.co.za>
2019-07-01 10:58:56 +0200

    123

--- ft_ls.c
+++ ft_ls.c
@@ -6,32 +6,36 @@
 /*   By: rhutchin <rhutchin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 09:11:50 by rhutchin          #+#    #+#             */
-/*   Updated: 2019/07/01 09:30:49 by rhutchin         ###   ########.fr       */
+/*   Updated: 2019/07/01 09:46:01 by rhutchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./incl/ft_ls.h"
-#include <dirent.h>
-#include <stdio.h> //---------------------------------------------------------- Delete me
+#include <dirent.h> //--------------------------------------------------------- Makes the DIR opening, closing and reading magic happen.
+#include <stdio.h> //---------------------------------------------------------- Delete me mc cheaty
+
+static void ft_reader()
+{
+    struct dirent *de;  //----------------------------------------------------- Pointer to the struct which will store the filename.
+    
+    DIR *dr = opendir(".");  //------------------------------------------------- returns a DIR pointer.
+  
+    if (dr == NULL) //--------------------------------------------------------- Breaks when nothing 
+    { 
+        printf("Could not open current directory" ); 
+    } 
+
+    while ((de = readdir(dr)) != NULL) 
+    {
+
+            printf("%s\n", de->d_name);
+    }
+  
+    closedir(dr);
+}
 
 int main (void)
 {
-    struct dirent *de;  // Pointer for directory entry 
-  
-    // opendir() returns a pointer of DIR type.  
-    DIR *dr = opendir("."); 
-  
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
-    { 
-        printf("Could not open current directory" ); 
-        return 0; 
-    } 
-  
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
-    // for readdir() 
-    while ((de = readdir(dr)) != NULL) 
-            printf("%s\n", de->d_name); 
-  
-    closedir(dr);     
-    return 0;    
+    ft_reader();
+    return (0);    
 }
