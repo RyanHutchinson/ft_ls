2ac0f4ccb7f114408b71072cda2e7c04aa7066dd
Ryan HUTCHINSON <rhutchin@student.wethinkcode.co.za>
2019-07-01 09:31:01 +0200

    Damn son

--- ft_ls.c
+++ ft_ls.c
@@ -6,15 +6,32 @@
 /*   By: rhutchin <rhutchin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 09:11:50 by rhutchin          #+#    #+#             */
-/*   Updated: 2019/07/01 09:19:43 by rhutchin         ###   ########.fr       */
+/*   Updated: 2019/07/01 09:30:49 by rhutchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./incl/ft_ls.h"
-#include <stdio.h>
+#include <dirent.h>
+#include <stdio.h> //---------------------------------------------------------- Delete me
 
 int main (void)
 {
-    printf("test\n");
-    return (0);    
+    struct dirent *de;  // Pointer for directory entry 
+  
+    // opendir() returns a pointer of DIR type.  
+    DIR *dr = opendir("."); 
+  
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
+    { 
+        printf("Could not open current directory" ); 
+        return 0; 
+    } 
+  
+    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
+    // for readdir() 
+    while ((de = readdir(dr)) != NULL) 
+            printf("%s\n", de->d_name); 
+  
+    closedir(dr);     
+    return 0;    
 }
